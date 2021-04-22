433a118f1f1a331c924cfcac36a1e01bd9808229
saratvk <saratavakoli77@ut.ac.ir>
2021-04-22 20:41:03 +0430

    feat: init store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 import LayoutResolver from '@/components/core/LayoutResolver';
 
 import Login from '@/views/Login';
@@ -6,17 +8,23 @@ import Register from '@/views/Register';
 import Home from '@/views/Home';
 
 import '@/styles/App.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <LayoutResolver path="/login" component={Login} layout="auth" />
-        <LayoutResolver path="/register" component={Register} layout="auth" />
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <LayoutResolver path="/login" component={Login} layout="auth" />
+          <LayoutResolver path="/register" component={Register} layout="auth" />
 
-        <LayoutResolver path="/" exact component={Home} />
-      </Switch>
-    </Router>
+          <LayoutResolver path="/" exact component={Home} />
+        </Switch>
+      </Router>
+
+      <ToastContainer />
+    </Provider>
   );
 }
 
