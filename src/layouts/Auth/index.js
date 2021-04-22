c500b477aa674a015ec39e5088a9b0536bac4109
saratvk <saratavakoli77@ut.ac.ir>
2021-04-22 20:40:51 +0430

    feat: update AuthLayout

--- src/layouts/Auth/index.js
+++ src/layouts/Auth/index.js
@@ -1,5 +1,16 @@
+import Footer from '@/components/layout/Footer';
+import Card from 'react-bootstrap/Card';
+import './styles.scss';
+
 const AuthLayout = ({ children }) => {
-  return <div className="AuthLayout">{children}</div>;
+  return (
+    <div className="AuthLayout">
+      <Card style={{ width: '25rem' }}>
+        <Card.Body>{children}</Card.Body>
+      </Card>
+      <Footer />
+    </div>
+  );
 };
 
 export default AuthLayout;
