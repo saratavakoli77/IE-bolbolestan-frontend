255157591a6fc1482b8c22c426a9d476c25585fa
saratvk <saratvk1377@gmail.com>
2021-04-22 16:51:06 +0430

    feat(default-layout): add Header and Footer

--- src/layouts/Default/index.js
+++ src/layouts/Default/index.js
@@ -1,5 +1,14 @@
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
 const DefaultLayout = ({ children }) => {
-  return <div className="DefaultLayout">{children}</div>;
+  return (
+    <div className="DefaultLayout">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 export default DefaultLayout;
