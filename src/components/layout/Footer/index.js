e70c59ad27d11b27e4603180bf781651d7982043
saratvk <saratavakoli77@ut.ac.ir>
2021-04-22 17:47:44 +0430

    feat(footer): update template

--- src/components/layout/Footer/index.js
+++ src/components/layout/Footer/index.js
@@ -1,5 +1,42 @@
+import AppIcon from '@/components/shared/AppIcon';
+import './styles.scss';
+
 const Footer = () => {
-  return <footer />;
+  return (
+    <footer className="footer fixed-bottom w-100 text-white py-3">
+      <div className="container d-flex align-items-center justify-content-between">
+        <p className="mb-0">&#9400; دانشگاه تهران - سامانه جامع بلبلستان</p>
+
+        <nav>
+          <ul className="d-flex align-items-center p-0 m-0">
+            <li className="d-flex">
+              <a href="/" className="d-inline-flex">
+                <AppIcon
+                  icon="twitter-logo-on-black-background"
+                  className="footer__icon"
+                />
+              </a>
+            </li>
+            <li className="d-flex">
+              <a href="/" className="d-inline-flex">
+                <AppIcon icon="facebook" className="footer__icon" />
+              </a>
+            </li>
+            <li className="d-flex">
+              <a href="/" className="d-inline-flex">
+                <AppIcon icon="instagram" className="footer__icon" />
+              </a>
+            </li>
+            <li className="d-flex">
+              <a href="/" className="d-inline-flex">
+                <AppIcon icon="linkedin-logo" className="footer__icon" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
