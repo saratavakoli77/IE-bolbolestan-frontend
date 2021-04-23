2ab7e1b0b4558cdd3607903aafefca0e00bf8cc5
saratvk <saratavakoli77@ut.ac.ir>
2021-04-23 17:26:17 +0430

    feat(api): add courses APIs

--- src/api/modules/courses.js
+++ src/api/modules/courses.js
@@ -1,8 +1,20 @@
 const COURSES_ENDPOINT = 'courses';
+const PICKED_COURSES_ENDPOINT = 'weekly_schedule';
+const PICK_COURSE_ENDPOINT = (code, classCode) =>
+  `courses/${code}/${classCode}`;
 
 const coursesModuleFactory = ({ $http }) => ({
   fetchAllCourses() {
     return $http.get(COURSES_ENDPOINT).then((res) => res.offeringEntities);
+  },
+  fetchPickedCourses() {
+    return $http.get(PICKED_COURSES_ENDPOINT).then((res) => ({
+      units: res.units,
+      courses: res.weeklyScheduleOfferings,
+    }));
+  },
+  pickCourse(code, classCode) {
+    return $http.post(PICK_COURSE_ENDPOINT(code, classCode));
   },
 });
 
