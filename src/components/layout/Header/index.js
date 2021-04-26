0245f8581425bf0bc5cf49b77d0f57804df910b7
saratvk <saratavakoli77@ut.ac.ir>
2021-04-26 20:42:36 +0430

    feat(header): add logout modal

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -6,6 +6,9 @@ import { useHistory, useRouteMatch } from 'react-router';
 import $api from '@/api';
 import { useDispatch } from 'react-redux';
 import { logout } from '@/store/slices/userSlice';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import AppBtn from '@/components/shared/AppBtn';
 
 const links = [
   {
@@ -26,6 +29,10 @@ const Header = () => {
   const match = useRouteMatch();
   const history = useHistory();
   const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const hideModal = () => setShowModal(false);
 
   const onLogout = () => {
     $api.auth.logout();
@@ -54,8 +61,22 @@ const Header = () => {
               label="خروج"
               icon="log-out"
               className="text-danger"
-              onClick={onLogout}
+              onClick={openModal}
             />
+
+            <Modal show={showModal} onHide={hideModal} centered>
+              <Modal.Body>
+                آیا میخواهید از حساب کاربری خود خارج شوید ؟
+              </Modal.Body>
+              <Modal.Footer>
+                <AppBtn variant="gray" outlined onClick={hideModal}>
+                  انصراف
+                </AppBtn>
+                <AppBtn variant="success" onClick={onLogout}>
+                  خروج
+                </AppBtn>
+              </Modal.Footer>
+            </Modal>
           </li>
         </ul>
       </nav>
